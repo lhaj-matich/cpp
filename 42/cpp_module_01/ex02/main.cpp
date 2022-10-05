952dd262a5a63d06573c8541a7ae8457dc7162b9
lhaj-matich <choumouloussama@gmail.com>
2022-10-05 17:12:01 +0100

    Finished exercise 02.

---
+++ 42/cpp_module_01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+
+    std::cout << "The memory address of string: " << &brain << std::endl;
+    std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+
+    std::cout << "brain Value: " << brain << std::endl;
+    std::cout << "balue pointed to by stringPTR: " << stringPTR << std::endl;
+    std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+
+    return (0);
+}
