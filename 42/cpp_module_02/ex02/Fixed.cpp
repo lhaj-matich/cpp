86edec444bd59ad34f861cdef0b39192a78d13ae
lhaj-matich <choumouloussama@gmail.com>
2022-10-17 18:02:19 +0100

    Working on the overloading operators.

--- 42/cpp_module_02/ex02/Fixed.cpp
+++ 42/cpp_module_02/ex02/Fixed.cpp
@@ -61,7 +61,132 @@ std::ostream &operator << (std::ostream &output,const Fixed &input)
     return output;
 }
 
-void    Fixed::operator>(const Fixed &rhs)
+bool    Fixed::operator> (const Fixed &rhs) const
 {
-    
+    if (this->fixed_value > rhs.fixed_value)
+        return (true);
+    return (false);
 }
+
+bool    Fixed::operator< (const Fixed &rhs) const
+{
+    if (this->getRawBits() > rhs.getRawBits())
+        return (true);
+    return (false);
+}
+
+bool    Fixed::operator<= (const Fixed &rhs) const
+{
+    if (this->getRawBits() <= rhs.getRawBits())
+        return (true);
+    return (false);
+}
+
+bool    Fixed::operator>= (const Fixed &rhs) const
+{
+    if (this->getRawBits() >= rhs.getRawBits())
+        return (true);
+    return (false);
+}
+
+bool    Fixed::operator== (const Fixed &rhs) const
+{
+    if (this->getRawBits() == rhs.getRawBits())
+        return (true);
+    return (false);
+}
+
+bool    Fixed::operator!= (const Fixed &rhs) const
+{
+    if (this->getRawBits() != rhs.getRawBits())
+        return (true);
+    return (false);
+}
+
+Fixed & Fixed::operator+(const Fixed &rhs)
+{
+    this->fixed_value = this->toFloat() + rhs.toFloat();
+    return (*this);
+}
+
+Fixed & Fixed::operator-(const Fixed &rhs)
+{
+    this->fixed_value = this->toFloat() - rhs.toFloat();
+    return (*this);
+}
+
+Fixed & Fixed::operator/(const Fixed &rhs)
+{
+    this->fixed_value = this->toFloat() / rhs.toFloat();
+    return (*this);
+}
+
+Fixed & Fixed::operator*(const Fixed &rhs)
+{
+    this->fixed_value = this->toFloat() * rhs.toFloat();
+    return (*this);
+}
+
+Fixed & Fixed::operator++(void)
+{
+    int i;
+
+    i = this->toInt();
+    this->fixed_value += ++i;
+    return (*this);
+}
+
+Fixed & Fixed::operator++(int)
+{
+    int i;
+
+    i = this->toInt();
+    this->fixed_value += i++;
+    return (*this);
+}
+
+Fixed & Fixed::operator--(void)
+{
+    int i;
+
+    i = this->toInt();
+    this->fixed_value = --i;
+    return (*this);
+}
+
+Fixed & Fixed::operator--(int)
+{
+    int i;
+
+    i = this->toInt();
+    this->fixed_value = i--;
+    return (*this);
+}
+
+static Fixed & min(Fixed & lhs, Fixed & rhs) 
+{
+    if (lhs.getRawBits() > rhs.getRawBits())
+        return (rhs);
+    return (lhs);
+}
+
+// static const Fixed & min(const Fixed &lhs, const Fixed &rhs) 
+// {
+//     if (lhs.getRawBits() > rhs.getRawBits())
+//         return (rhs);
+//     return (lhs);
+// }
+
+static Fixed & max(Fixed &lhs, Fixed &rhs) 
+{
+    if (lhs.getRawBits() > rhs.getRawBits())
+        return (lhs);
+    return (rhs);
+}
+
+// static const Fixed & max(const Fixed &lhs,const Fixed &rhs) 
+// {
+//     if (lhs.getRawBits() > rhs.getRawBits())
+//         return (lhs);
+//     return (rhs);
+// }
--- 42/cpp_module_02/ex02/Fixed.hpp
+++ 42/cpp_module_02/ex02/Fixed.hpp
@@ -20,6 +20,28 @@ class Fixed
         int     toInt(void) const;
         void    setRawBits(int raw);
         float   toFloat(void) const;
+        // Comparason operators.
+        bool    operator>(const Fixed &rhs) const;
+        bool    operator<(const Fixed &rhs) const;
+        bool    operator<= (const Fixed &rhs) const;
+        bool    operator>= (const Fixed &rhs) const;
+        bool    operator== (const Fixed &rhs) const;
+        bool    operator!= (const Fixed &rhs) const;
+        // Arithmetic operators.
+        Fixed & operator+(const Fixed &rhs);
+        Fixed & operator-(const Fixed &rhs);
+        Fixed & operator/(const Fixed &rhs);
+        Fixed & operator*(const Fixed &rhs);
+
+        // Increment and decrement operators.
+        Fixed & operator++(int);
+        Fixed & operator++();
+        Fixed & operator--(int);
+        Fixed & operator--();
+        static Fixed & min(Fixed & lhs, Fixed & rhs);
+        // static const Fixed & min(const Fixed & lhs, const Fixed & rhs);
+        // static const Fixed & max(const Fixed & lhs, const Fixed & rhs);
+        static Fixed & max(Fixed & lhs, Fixed & rhs);
 };
 
 std::ostream &operator << (std::ostream &output,const Fixed &input);
