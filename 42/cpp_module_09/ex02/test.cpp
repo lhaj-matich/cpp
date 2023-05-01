91aaf4d5585d49cbd3ef7f34a2d7dda6d58a5c3c
lhaj-matich <choumouloussama@gmail.com>
2023-05-01 06:47:01 +0100

    Added the selection sort but commented it for analysis.

--- 42/cpp_module_09/ex02/test.cpp
+++ 42/cpp_module_09/ex02/test.cpp
@@ -31,6 +31,9 @@
 //     // This merger function must be using insertion sort.
 // }
 
+void    printVector(std::vector<int> input);
+void    insertionSort(std::vector<int>& input);
+std::vector<int> mergeSort(std::vector<int> input);
 
 std::vector<int> merge(std::vector<int> leftArray, std::vector<int> rightArray)
 {
@@ -38,7 +41,6 @@ std::vector<int> merge(std::vector<int> leftArray, std::vector<int> rightArray)
     std::vector<int>::iterator j;
     std::vector<int> result;
 
-    std::cout << "Before seg" << std::endl;
 
     i = leftArray.begin();
     j = rightArray.begin();
@@ -46,11 +48,15 @@ std::vector<int> merge(std::vector<int> leftArray, std::vector<int> rightArray)
     while (i != leftArray.end() && j != rightArray.end())
     {
         if (*i <= *j)
+        {
             result.push_back(*i);
+            i++;
+        }
         else
+        {
             result.push_back(*j);
-        i++;
-        j++;
+            j++;
+        }
     }
     while (i != leftArray.end())
     {
@@ -62,18 +68,39 @@ std::vector<int> merge(std::vector<int> leftArray, std::vector<int> rightArray)
         result.push_back(*j);
         j++;
     }
-
+    return (result);
 }
+
+void insertionSort(std::vector<int>& input)
+{
+    for (int i = 1; i < input.size(); i++)
+    {
+        int key = input[i];
+        int j = i - 1;
+        while (j >= 0 && input[j] > key)
+        {
+            input[j + 1] = input[j];
+            j--;
+        }
+        input[j + 1] = key;
+    }
+}
+
 
 std::vector<int> mergeSort(std::vector<int> input)
 {
-    std::cout << "Size: " << input.size() << std::endl;
+    // std::cout << "Size: " << input.size() << std::endl;
     std::vector<int>::iterator middle;
 
     if (input.size() == 1)
         return input;
+    // if (input.size() <= 2) // Use insertion sort for small subarrays with a threshold of 2
+    // {
+    //     insertionSort(input);
+    //     return input;
+    // }
 
-    middle = input.begin() + input.size() / 2; 
+    middle = input.begin() + input.size() / 2;
 
     std::vector<int> leftVector =  mergeSort(std::vector<int>(input.begin(), middle));
     std::vector<int> rightVector = mergeSort(std::vector<int>(middle, input.end()));
@@ -81,11 +108,25 @@ std::vector<int> mergeSort(std::vector<int> input)
 }
 
 
+void    printVector(std::vector<int> input)
+{
+     std::vector<int>::iterator print;
+
+    print = input.begin();
+
+    while (print != input.end())
+    {
+        std::cout << *print << " ";
+        print++; 
+    }
+    std::cout << std::endl;
+}
+
 int main(int argc, char **argv)
 {
     std::vector<int> data = {9, 14, 4, 6, 5, 8, 7};
     std::vector<int> result = mergeSort(data);
 
-    std::cout << "Result size: " << result.size() << std::endl;
+    printVector(result);
     return (0);
 }
