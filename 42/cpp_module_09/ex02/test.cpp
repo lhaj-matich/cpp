4b7b79f344e20742c7d435a5750e562465fd7e15
lhaj-matich <choumouloussama@gmail.com>
2023-05-01 05:58:51 +0100

    Trying to implement the merge sort with vectors.

---
+++ 42/cpp_module_09/ex02/test.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+
+// Split
+
+/*
+    function ListNode sortList(ListNode head)
+    {
+        + This is in case i have only one 1 item or 0 item.
+        if (head == NULL || head.next == NULL)
+            return head;
+
+        + Here we need to get a refrence to the middle node.
+        mid = size(head) / 2;
+
+        ListNode leftSorted =  sortList(head);
+        ListNode rightSorted = sortlist(mid);
+
+        return merge(leftSorted, rightSorted);
+    }
+
+*/
+
+
+
+// Merge
+
+// int *merge(int *leftArray, int *rightArray)
+// {
+//     // This merger function must be using insertion sort.
+// }
+
+
+std::vector<int> merge(std::vector<int> leftArray, std::vector<int> rightArray)
+{
+    std::vector<int>::iterator i;
+    std::vector<int>::iterator j;
+    std::vector<int> result;
+
+    std::cout << "Before seg" << std::endl;
+
+    i = leftArray.begin();
+    j = rightArray.begin();
+
+    while (i != leftArray.end() && j != rightArray.end())
+    {
+        if (*i <= *j)
+            result.push_back(*i);
+        else
+            result.push_back(*j);
+        i++;
+        j++;
+    }
+    while (i != leftArray.end())
+    {
+        result.push_back(*i);
+        i++;
+    }
+    while (j != rightArray.end())
+    {
+        result.push_back(*j);
+        j++;
+    }
+
+}
+
+std::vector<int> mergeSort(std::vector<int> input)
+{
+    std::cout << "Size: " << input.size() << std::endl;
+    std::vector<int>::iterator middle;
+
+    if (input.size() == 1)
+        return input;
+
+    middle = input.begin() + input.size() / 2; 
+
+    std::vector<int> leftVector =  mergeSort(std::vector<int>(input.begin(), middle));
+    std::vector<int> rightVector = mergeSort(std::vector<int>(middle, input.end()));
+    return merge(leftVector, rightVector);
+}
+
+
+int main(int argc, char **argv)
+{
+    std::vector<int> data = {9, 14, 4, 6, 5, 8, 7};
+    std::vector<int> result = mergeSort(data);
+
+    std::cout << "Result size: " << result.size() << std::endl;
+    return (0);
+}
