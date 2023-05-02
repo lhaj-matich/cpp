4bf783d02a26477d6ae1372236429c3bb056bd91
lhaj-matich <choumouloussama@gmail.com>
2023-05-02 03:40:54 +0100

    Completed parsing the number insertion.

--- 42/cpp_module_09/ex02/PmergeMe.cpp
+++ 42/cpp_module_09/ex02/PmergeMe.cpp
@@ -19,6 +19,64 @@ PmergeMe::~PmergeMe()
 	std::cout << "\e[0;31mDestructor called of PmergeMe\e[0m" << std::endl;
 }
 
+bool	PmergeMe::checkNumber(char *str)
+{
+	size_t i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		if (!std::isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+bool	PmergeMe::checkInput(char **input, int size)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (std::atoi(input[i]) < 0)
+			throw PmergeMeError("The program cannot have negative numbers.");
+		else if (!checkNumber(input[i]))
+			throw PmergeMeError("Invalid numbers set.");
+		i++;
+	}
+}
+
+void	PmergeMe::insertNumbers(char **argv, int argc)
+{
+	size_t i;
+
+	i = 0;
+	_vec_parse_start_time = clock();
+	while (i < argc)
+		_vecnumbers.push_back(std::atoi(argv[i++]));
+	_vec_parse_end_time = clock();
+	i = 0;
+	_dec_parse_start_time = clock();
+	while (i < argc)
+		_deqnumbers.push_back(std::atoi(argv[i++]));
+	_dec_parse_end_time = clock();
+}
+
+
+void	PmergeMe::printUnsorted(char **argv, int argc)
+{
+	size_t i;
+
+	i = 0;
+
+	std::cout << "Before: ";
+	while (i < argc)
+		std::cout << argv[i++] << " ";
+	std::cout << std::endl;
+}
 
 // Operators
 PmergeMe & PmergeMe::operator=(const PmergeMe &assign)
--- 42/cpp_module_09/ex02/PmergeMe.hpp
+++ 42/cpp_module_09/ex02/PmergeMe.hpp
@@ -3,6 +3,9 @@
 
 # include <iostream>
 # include <string>
+# include <vector>
+# include <deque>
+# include <ctime>
 
 class PmergeMe
 {
@@ -16,10 +19,35 @@ class PmergeMe
 		
 		// Operators
 		PmergeMe & operator=(const PmergeMe &assign);
+
+		void	SortNumbers(char **, int);
 		
 	private:
-		
-		
+		std::deque<int>		_deqnumbers;
+		std::vector<int>	_vecnumbers;
+		// Sort time variables
+		clock_t				_dec_sort_start_time;
+		clock_t				_vec_sort_start_time;
+		// Parse time variables
+		clock_t				_dec_parse_start_time;
+		clock_t				_dec_parse_end_time;
+		clock_t				_vec_parse_start_time;
+		clock_t				_vec_parse_end_time;
+
+		bool	checkNumber(char *);
+		bool	checkInput(char **, int);
+		void	insertNumbers(char **, int);
+		void	printUnsorted(char **, int);
+		void	printSortedDeque();
+		void	printSortedVector();
+};
+
+class PmergeMeError: public std::runtime_error
+{
+    public:
+        PmergeMeError (std::string const& msg):
+            std::runtime_error(msg)
+        {}
 };
 
 #endif
